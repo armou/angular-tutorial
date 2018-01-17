3a18cbc2c7f730cfff9196fe6b4b88efc685f52c
aoudin <armel.oudin@dirox.net>
2018-01-17 12:04:24 +0700

    WIP: Reached #7 - Routing

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Rikimaru' },
+  { id: 12, name: 'Lina' },
+  { id: 13, name: 'Shadow Fiend' },
+  { id: 14, name: 'Magnus' },
+  { id: 15, name: 'Juggernaut' },
+  { id: 16, name: 'Alchemist' },
+  { id: 17, name: 'Techies' },
+  { id: 18, name: 'Treant Protector' },
+  { id: 19, name: 'Monkey King' },
+  { id: 20, name: 'Sven' }
+];
