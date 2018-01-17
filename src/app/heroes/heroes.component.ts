6ae9f3c4fc3f86c976f77d17ad7192c7a6ff981a
aoudin <armel.oudin@dirox.net>
2018-01-17 14:12:15 +0700

    WIP - reached #7 - Routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -24,7 +24,7 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  
+
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
