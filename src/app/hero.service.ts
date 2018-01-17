aa07f1ed1333720d9e8a481cc5642ac4e8e18917
aoudin <armel.oudin@dirox.net>
2018-01-17 16:24:15 +0700

    WIP : Reached #8 - HTTP

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MessageService } from './message.service';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
@@ -8,7 +8,9 @@ import { of } from 'rxjs/observable/of';
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
 
   getHeroes(): Observable<Hero[]> {
